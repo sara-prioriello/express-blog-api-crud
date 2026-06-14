9e9700e0bd354040e227c905e3ecc2d3828d037c
sara-prioriello <saraprioriello6@gmail.com>
2026-06-14 17:48:52 +0200

    milestone4-part2

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -63,7 +63,33 @@ function store(req, res) {
 }
 
 function update(req, res) {
-    res.send(`Aggiornamento del post con id ${req.params.id}`);
+    //res.send(`Aggiornamento del post con id ${req.params.id}`);
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il post tramite id
+    const post = posts.find(post => post.id === id);
+
+    // Piccolo controllo
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    // Aggiorniamo il post con i dati ricevuti
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post.image = req.body.image;
+    post.tags = req.body.tags;
+
+    // Restituiamo lo status corretto e il post aggiornato
+    res.json(post);
+
 }
 
 function modify(req, res) {
